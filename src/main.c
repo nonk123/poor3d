1e436d88b6133fc833ecb088daeccd39571c1821
nonk123 <me@nonk.dev>
2026-02-25 16:41:52 +0300

    don't need the vertex/face counter actually

--- src/main.c
+++ src/main.c
@@ -16,13 +16,11 @@ int main(int argc, char* argv[]) {
 	float radius = 2.f, angle = 0.f;
 
 	WHILE_POOR {
-		Mesh* mesh = find_mesh("assets/penger-no-hull.obj");
-		mesh->pos.y = -0.5f;
-
-		poor_printf(0, 0, "V%d : F%d", mesh->vcount, mesh->fcount);
 		look_at(XYZ(cosf(angle) * radius, 0.f, sinf(angle) * radius), ORIGIN);
 		angle += 0.01f;
 
+		Mesh* mesh = find_mesh("assets/penger-no-hull.obj");
+		mesh->pos.y = -0.5f;
 		draw_mesh(mesh);
 
 		if (poor_key_down(POOR_ESC))
